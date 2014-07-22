3075dc0db565968344e37cccb0984774e856aec7
William Kennedy <bill@ardanstudios.com>
2014-07-22 01:02:10 -0400

    Updated comments.

--- 05-standard_library/writers_readers/example2/example2.go
+++ 05-standard_library/writers_readers/example2/example2.go
@@ -1,28 +1,3 @@
-/*
-http://golang.org/pkg/bytes/#NewBufferString
-NewBufferString creates and initializes a new Buffer using string
-s as its initial contents. It is intended to prepare a buffer to
-read an existing string.
-
-func NewBufferString(s string) *Buffer
-
-*******
-
-http://golang.org/pkg/encoding/base64/#NewDecoder
-NewDecoder constructs a new base64 stream decoder.
-
-func NewDecoder(enc *Encoding, r io.Reader) io.Reader
-
-*******
-
-http://golang.org/pkg/io/#Copy
-Copy copies from src to dst until either EOF is reached on src or
-an error occurs. It returns the number of bytes copied and the first
-error encountered while copying, if any.
-
-func Copy(dst Writer, src Reader) (written int64, err error)
-*/
-
 // Sample program to show how a string can be converted to an
 // io.Reader and used with functions that support the inteface.
 package main
