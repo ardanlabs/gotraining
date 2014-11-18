acde98da35c240e1f1d762e693d2d8c2e23c45a7
William Kennedy <bill@ardanstudios.com>
2014-11-18 12:54:24 -0500

    Corrected example to use the errors package.

--- 09-error_handling/example2/example2.go
+++ 09-error_handling/example2/example2.go
@@ -1,41 +1,23 @@
 // All material is licensed under the GNU Free Documentation License
 // https://github.com/ArdanStudios/gotraining/blob/master/LICENSE
 
-// http://play.golang.org/p/kKVIMMDpjb
+// http://play.golang.org/p/Zo1o0LFThp
 
 // Sample program to show how to use error variables to help the
 // caller determine the exact error being returned.
 package main
 
-import "fmt"
-
-// http://golang.org/pkg/builtin/#error
-type error interface {
-	Error() string
-}
-
-// http://golang.org/src/pkg/errors/errors.go
-type errorString struct {
-	s string
-}
-
-// http://golang.org/src/pkg/errors/errors.go
-func (e *errorString) Error() string {
-	return e.s
-}
-
-// http://golang.org/src/pkg/errors/errors.go
-// New returns an error that formats as the given text.
-func New(text string) error {
-	return &errorString{text}
-}
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	// BadRequestError is returned when there are problems with the request.
-	BadRequestError = New("Bad Request")
+	BadRequestError = errors.New("Bad Request")
 
 	// MovedPermanentlyError is returned when a 301/302 is returned.
-	MovedPermanentlyError = New("Moved Permanently")
+	MovedPermanentlyError = errors.New("Moved Permanently")
 )
 
 // main is the entry point for the application.
