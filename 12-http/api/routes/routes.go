f575ec712d599452c2f694bfa510dabd08d1bb33
William Kennedy <bill@ardanstudios.com>
2015-04-19 17:28:56 -0700

    Integrated AngularJS to bind to the API

--- 12-http/api/routes/routes.go
+++ 12-http/api/routes/routes.go
@@ -10,10 +10,14 @@ import (
 // API returns a handler for a set of routes.
 func API() http.Handler {
 	a := app.New()
+
+	a.TreeMux.NotFoundHandler = http.FileServer(http.Dir("views")).ServeHTTP
+
 	a.Handle("GET", "/v1/users", handlers.Users.List)
 	a.Handle("POST", "/v1/users", handlers.Users.Create)
 	a.Handle("GET", "/v1/users/:id", handlers.Users.Retrieve)
 	a.Handle("PUT", "/v1/users/:id", handlers.Users.Update)
 	a.Handle("DELETE", "/v1/users/:id", handlers.Users.Delete)
+
 	return a
 }
