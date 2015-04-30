531190f93beab1443ffc962cefd57ff66acb5ae3
William Kennedy <bill@ardanstudios.com>
2015-04-30 15:33:35 -0400

    Trimmed down example.

--- 09-testing/01-testing/example2/example2_test.go
+++ 09-testing/01-testing/example2/example2_test.go
@@ -20,45 +20,29 @@ const failed = "\u2717"
 
 // feed is mocking the XML document we except to receive.
 var feed = `<?xml version="1.0" encoding="UTF-8"?>
-<rss xmlns:atom="http://www.w3.org/2005/Atom"
-     xmlns:openSearch="http://a9.com/-/spec/opensearchrss/1.0/" 
-     xmlns:blogger="http://schemas.google.com/blogger/2008"
-     xmlns:georss="http://www.georss.org/georss"
-     xmlns:gd="http://schemas.google.com/g/2005"
-     xmlns:thr="http://purl.org/syndication/thread/1.0" version="2.0">
+<rss>
 <channel>
-    <atom:id>tag:blogger.com,1999:blog-6298089572270107210</atom:id>
-    <lastBuildDate>Mon, 27 Apr 2015 12:42:23 +0000</lastBuildDate>
     <title>Going Go Programming</title>
-    <description>Golang For The Common Man : https://github.com/goinggo</description>
+    <description>Golang : https://github.com/goinggo</description>
     <link>http://www.goinggo.net/</link>
-    <managingEditor>noreply@blogger.com (William Kennedy)</managingEditor>
-    <generator>Blogger</generator>
-    <openSearch:totalResults>67</openSearch:totalResults>
-    <openSearch:startIndex>1</openSearch:startIndex>
-    <openSearch:itemsPerPage>25</openSearch:itemsPerPage>
     <item>
-        <guid isPermaLink="false">tag:blogger.com,1999:blog-6298089572270107210.post-3707264680032475251</guid>
         <pubDate>Sun, 15 Mar 2015 15:04:00 +0000</pubDate>
-        <atom:updated>2015-03-15T11:21:09.934-04:00</atom:updated>
         <title>Object Oriented Programming Mechanics</title>
-        <description>&lt;div class="p1"&gt;Go is an object oriented programming language. It may not have inheritance, but in this 20 minute video from the Bangalore meetup, I will show you how object oriented programming practices and techniques can be applied to your Go programs. From an object oriented standpoint, Go does provides the ability to add behavior to your types via methods, allows you to implement polymorphic behavior via interfaces and gives you a way to extend the state and behavior of any existing type via type embedding. Go also provides a form of encapsulation that allows your types, including their fields and methods, to be visible or invisible. Everything you need to write object oriented programs is available in Go.&lt;/div&gt;&lt;br /&gt;&lt;iframe allowfullscreen="" frameborder="0" height="510" src="https://www.youtube.com/embed/gRpUfjTwSOo" width="100%"&gt;&lt;/iframe&gt; &lt;br /&gt;&lt;b&gt;&lt;br /&gt;&lt;/b&gt;&lt;b&gt;Example 1 - Methods: &lt;/b&gt;&lt;br /&gt;&lt;a href="http://play.golang.org/p/hfRkC6nKag" target="_blank"&gt;http://play.golang.org/p/hfRkC6nKag&lt;/a&gt;&lt;br /&gt;&lt;br /&gt;&lt;b&gt;Example 2 - Interfaces:&lt;/b&gt;&lt;br /&gt;&lt;a href="http://play.golang.org/p/F1UyKlTh3k" target="_blank"&gt;http://play.golang.org/p/F1UyKlTh3k&lt;/a&gt;&lt;br /&gt;&lt;br /&gt;&lt;b&gt;Example 3 - Extending Types:&lt;/b&gt;&lt;br /&gt;&lt;a href="http://play.golang.org/p/JJ811lBwoz" target="_blank"&gt;http://play.golang.org/p/JJ811lBwoz&lt;/a&gt;&lt;br /&gt;&lt;br /&gt;&lt;b&gt;Example 4 - Overriding Inner Types:&lt;/b&gt;&lt;br /&gt;&lt;a href="http://play.golang.org/p/-xQFBv9_82" target="_blank"&gt;http://play.golang.org/p/-xQFBv9_82&lt;/a&gt;</description>
-        <link>http://www.goinggo.net/2015/03/object-oriented-programming-mechanics.html</link>
-        <author>noreply@blogger.com (William Kennedy)</author>
-        <media:thumbnail xmlns:media="http://search.yahoo.com/mrss/" url="http://img.youtube.com/vi/gRpUfjTwSOo/default.jpg" height="72" width="72"/>
-        <thr:total>0</thr:total>
+        <description>Go is an object oriented language.</description>
+        <link>http://www.goinggo.net/2015/03/object-oriented</link>
     </item>
 </channel>
 </rss>`
 
 // mockServer returns a pointer to a server to handle the mock get call.
 func mockServer() *httptest.Server {
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	f := func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "application/xml")
 		fmt.Fprintln(w, feed)
-	}))
-	return server
+	}
+
+	return httptest.NewServer(http.HandlerFunc(f))
 }
 
 // TestDownload tests if download web content is working.
