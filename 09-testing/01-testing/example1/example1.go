2510a5e2a0b33adbf69a3e73c8163c284df36342
William Kennedy <bill@ardanstudios.com>
2015-04-30 15:14:23 -0400

    Trimmed down the RSS types.

--- 09-testing/01-testing/example1/example1.go
+++ 09-testing/01-testing/example1/example1.go
@@ -11,38 +11,20 @@ import "encoding/xml"
 // the buoy RSS document.
 type Item struct {
 	XMLName     xml.Name `xml:"item"`
-	PubDate     string   `xml:"pubDate"`
 	Title       string   `xml:"title"`
 	Description string   `xml:"description"`
 	Link        string   `xml:"link"`
-	GUID        string   `xml:"guid"`
-	GeoRssPoint string   `xml:"georss:point"`
-}
-
-// Image defines the fields associated with the image tag in
-// the buoy RSS document.
-type Image struct {
-	XMLName xml.Name `xml:"image"`
-	URL     string   `xml:"url"`
-	Title   string   `xml:"title"`
-	Link    string   `xml:"link"`
 }
 
 // Channel defines the fields associated with the channel tag in
 // the buoy RSS document.
 type Channel struct {
-	XMLName        xml.Name `xml:"channel"`
-	Title          string   `xml:"title"`
-	Description    string   `xml:"description"`
-	Link           string   `xml:"link"`
-	PubDate        string   `xml:"pubDate"`
-	LastBuildDate  string   `xml:"lastBuildDate"`
-	TTL            string   `xml:"ttl"`
-	Language       string   `xml:"language"`
-	ManagingEditor string   `xml:"managingEditor"`
-	WebMaster      string   `xml:"webMaster"`
-	Image          Image    `xml:"image"`
-	Items          []Item   `xml:"item"`
+	XMLName     xml.Name `xml:"channel"`
+	Title       string   `xml:"title"`
+	Description string   `xml:"description"`
+	Link        string   `xml:"link"`
+	PubDate     string   `xml:"pubDate"`
+	Items       []Item   `xml:"item"`
 }
 
 // Document defines the fields associated with the buoy RSS document.
