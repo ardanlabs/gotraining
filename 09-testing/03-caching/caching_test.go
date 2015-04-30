dc57c4597da9da28f810bd9670a284d14f1b5c60
William Kennedy <bill@ardanstudios.com>
2015-04-30 15:38:36 -0400

    Added missing comments

--- 09-testing/03-caching/caching_test.go
+++ 09-testing/03-caching/caching_test.go
@@ -10,12 +10,16 @@ package caching
 
 import "testing"
 
+// BenchmarkRowTraverse capture the time it takes to perform
+// a row traversal.
 func BenchmarkRowTraverse(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		rowTraverse()
 	}
 }
 
+// BenchmarkColTraverse capture the time it takes to perform
+// a column traversal.
 func BenchmarkColTraverse(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		colTraverse()
