0c0c6a6d813a1ac74177dce9d40fe939d5d18cf8
Kevin Gillette <extemporalgenome@gmail.com>
2015-05-03 09:57:07 -0600

    errors should not end in a period. other formatting
    param names in errors should also match the code case; English
    capitalization rules do not take precedence here

--- 08-concurrency_patterns/pool/pool.go
+++ 08-concurrency_patterns/pool/pool.go
@@ -25,14 +25,14 @@ type Pool struct {
 
 // ErrPoolClosed is returned when an Acquire returns on a
 // closed pool.
-var ErrPoolClosed = errors.New("Pool has been closed.")
+var ErrPoolClosed = errors.New("Pool has been closed")
 
 // New creates a pool that manages resources. A pool requires a
 // function that can allocate a new resource and the size of
 // the pool.
 func New(size uint, f func() (io.Closer, error)) (*Pool, error) {
 	if size == 0 {
-		return nil, errors.New("Size value too small.")
+		return nil, errors.New("size value too small")
 	}
 
 	return &Pool{
