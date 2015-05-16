b554da5bf4382610f298822d678f2bb9d3f31e7f
William Kennedy <bill@ardanstudios.com>
2015-05-16 08:55:03 -0400

    Removed a comment.

--- 09-testing/01-testing/example4/handlers/handlers_example_test.go
+++ 09-testing/01-testing/example4/handlers/handlers_example_test.go
@@ -1,8 +1,6 @@
 // All material is licensed under the GNU Free Documentation License
 // https://github.com/ArdanStudios/gotraining/blob/master/LICENSE
 
-// localhost:4000/pkg/github.com/ArdanStudios/gotraining/09-testing/01-testing/example4/handlers/
-
 // Sample test to show how to write a basic example test.
 package handlers_test
 
