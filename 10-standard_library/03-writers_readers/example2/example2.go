e5a62bbc34330e99699c5a763822833d372080e8
Kevin Gillette <extemporalgenome@gmail.com>
2015-05-16 13:14:51 -0400

    curl stdlib example: more idiomatic

--- 10-standard_library/03-writers_readers/example2/example2.go
+++ 10-standard_library/03-writers_readers/example2/example2.go
@@ -1,11 +1,11 @@
 // All material is licensed under the GNU Free Documentation License
 // https://github.com/ArdanStudios/gotraining/blob/master/LICENSE
-
-// http://play.golang.org/p/W3YoitIiT-
-
+//
+// http://play.golang.org/p/B16Qb9a46S
+//
 // https://gist.github.com/jmoiron/e9f72720cef51862b967#file-01-curl-go
 // Sample code provided by Jason Moiron
-
+//
 // ./example2 http://www.goinggo.net/feeds/posts/default
 
 // Sample program to show how to write a simple version of curl using
@@ -29,16 +29,17 @@ func init() {
 
 // main is the entry point for the application.
 func main() {
-	// r here is a response, and r.Body is an io.Reader
-	r, err := http.Get(os.Args[1])
+	// resp here is a response, and resp.Body is an io.Reader
+	resp, err := http.Get(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer resp.Body.Close()
 
 	// io.Copy(dst io.Writer, src io.Reader), copies from the Body to Stdout
-	io.Copy(os.Stdout, r.Body)
-	if err := r.Body.Close(); err != nil {
+	_, err = io.Copy(os.Stdout, resp.Body)
+	if err != nil {
 		fmt.Println(err)
 	}
 }
