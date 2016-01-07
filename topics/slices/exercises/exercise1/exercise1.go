a52e46f4b5367e97f2abca97350fae20aff9007b
William Kennedy <bill@ardanlans.com>
2016-01-07 08:51:50 -0500

    Updated new playground link inside code.

--- topics/slices/exercises/exercise1/exercise1.go
+++ topics/slices/exercises/exercise1/exercise1.go
@@ -1,7 +1,7 @@
 // All material is licensed under the Apache License Version 2.0, January 2004
 // http://www.apache.org/licenses/LICENSE-2.0
 
-// https://play.golang.org/p/uJrY2JZApJ
+// https://play.golang.org/p/HaCD5pxCMZ
 
 // Declare a nil slice of integers. Create a loop that appends 10 values to the
 // slice. Iterate over the slice and display each value.
