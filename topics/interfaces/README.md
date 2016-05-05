03334fe5568fe5f263bbb0f829a2d34d29a5a549
William Kennedy <bill@ardanlabs.com>
2016-05-04 20:54:59 -0700

    Updated the example with better types.

--- topics/interfaces/README.md
+++ topics/interfaces/README.md
@@ -33,7 +33,7 @@ http://www.goinggo.net/2014/05/methods-interfaces-and-embedded-types.html
 
 ## Advanced Code Review
 
-[Storing Values](advanced/example1/example1.go) ([Go Playground](http://play.golang.org/p/nInf73lwqz))
+[Storing Values](advanced/example1/example1.go) ([Go Playground](https://play.golang.org/p/r9HYjCUzUx))
 
 ## Exercises
 
--- topics/interfaces/advanced/example1/example1.go
+++ topics/interfaces/advanced/example1/example1.go
@@ -10,17 +10,19 @@ import (
 	"unsafe"
 )
 
-// biller is a random interface.
-type biller interface {
-	bill()
+// notifier provides support for notifying events.
+type notifier interface {
+	notify()
 }
 
-// value is a concrete type that will implement the interface.
-type value int
+// user represents a user in the system.
+type user struct {
+	name string
+}
 
-// bill implements the biller interface.
-func (v value) bill() {
-	fmt.Println(v)
+// notify implements the notifier interface.
+func (u user) notify() {
+	fmt.Println("Alert", u.name)
 }
 
 func main() {
@@ -28,40 +30,40 @@ func main() {
 	// Capture the size of a word in this arch.
 	size := unsafe.Sizeof(10)
 
-	// Create a biller interface and concrete type value.
-	var b biller
-	v := value(10)
+	// Create a notifier interface and concrete type value.
+	var n1 notifier
+	u := user{"bill"}
 
-	// The assignment stores a copy of the value inside
-	// the interface value.
-	b = v
+	// The assignment stores a copy of the user value inside
+	// the notifier interface value.
+	n1 = u
 
 	// Get a pointer to the second word of the interface value.
 	// We want to inspect the value that the interface is pointing to.
-	second := uintptr(unsafe.Pointer(&b)) + uintptr(size)
-	valPtr := (**value)(unsafe.Pointer(second))
-	fmt.Println("Addr V:", &v, "Int Addr:", *valPtr, "Value:", **valPtr)
+	word := uintptr(unsafe.Pointer(&n1)) + uintptr(size)
+	value := (**user)(unsafe.Pointer(word))
+	fmt.Printf("N1: Addr User: %p  Word Value: %p  Ptr Value: %v\n", &u, *value, **value)
 
 	// Create a second interface value and assign the orginal interface
 	// value to the new interface value.
-	var b1 biller
-	b1 = b
+	var n2 notifier
+	n2 = n1
 
 	// Get a pointer to the second word of the interface value.
 	// We want to inspect the value that the interface is pointing to.
-	second = uintptr(unsafe.Pointer(&b1)) + uintptr(size)
-	valPtr = (**value)(unsafe.Pointer(second))
-	fmt.Println("Addr V:", &v, "Int Addr:", *valPtr, "Value:", **valPtr)
+	word = uintptr(unsafe.Pointer(&n2)) + uintptr(size)
+	value = (**user)(unsafe.Pointer(word))
+	fmt.Printf("N2: Addr User: %p  Word Value: %p  Ptr Value: %v\n", &u, *value, **value)
 
-	// Change the value that the interface is pointing to.
-	**valPtr = 11
+	// Mutate the value that the interface is pointing to.
+	(**value).name = "lisa"
 
 	// We see the change in both interface values. What this means is
 	// that when we make an assignment between different interface values,
 	// like on line 42, we are copying just the interface value. We are not making
 	// a second copy of the value that was stored. Both b and b1 are pointing to
 	// the same value we originally stored on line 26.
-	fmt.Println(b.(value), b1.(value))
+	fmt.Println("N1:", n1.(user), "N2:", n2.(user))
 
 	// The type assertion is returning a copy of the value stored. This is
 	// whether the value was an address or not.
