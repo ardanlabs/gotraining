e080a0836da32b4f129d3ba1ef3f488c48e1223f
William Kennedy <bill@ardanlabs.com>
2016-09-06 18:05:29 -0400

    Changed the basic test based on code from Florin Patan. Using a square matrix for the caching example.

--- topics/benchmarking/caching/caching.go
+++ topics/benchmarking/caching/caching.go
@@ -1,16 +1,16 @@
 // All material is licensed under the Apache License Version 2.0, January 2004
 // http://www.apache.org/licenses/LICENSE-2.0
 
-// package caching provides code to show why Data Oriented Design matters. How
+// Package caching provides code to show why Data Oriented Design matters. How
 // data layouts matter more to performance than algorithm efficiency.
 package caching
 
 import "fmt"
 
-// Set the size of each row to be 64k.
+// Create a square matrix of 2 meg by 2 meg.
 const (
-	rows = 64
-	cols = 64 * 1024
+	rows = 2 * 1024
+	cols = 2 * 1024
 )
 
 // matrix represents a matrix with a large number of
@@ -29,7 +29,7 @@ var list *data
 func init() {
 	var last *data
 
-	// Set ~13% of the matrix to 0XFF.
+	// Create a link list with the same number of elements.
 	for row := 0; row < rows; row++ {
 		for col := 0; col < cols; col++ {
 
@@ -43,8 +43,8 @@ func init() {
 			}
 			last = &d
 
-			// Apply a value on this interval.
-			if row%8 == 0 {
+			// Add a value to all even elements.
+			if row%2 == 0 {
 				matrix[row][col] = 0xFF
 				d.v = 0xFF
 			}
