77773897077420005550ffb3bb1dbd074e0ee6bc
William Kennedy <bill@ardanlabs.com>
2016-11-03 14:06:03 -0400

    Refactored the consuming topic.

--- topics/web/consuming/example6/jwt.go
+++ topics/web/consuming/example6/jwt.go
@@ -1,3 +1,6 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package main
 
 import (
@@ -8,21 +11,35 @@ import (
 	jose "github.com/dvsekhvalnov/jose2go"
 )
 
+// JWTTransporter provides a custom transporter for making requests
+// with JWT authentication.
 type JWTTransporter struct {
 	transporter  http.RoundTripper
 	sharedSecret []byte
 }
 
+// RoundTrip implements the RoundTripper interface.
 func (c *JWTTransporter) RoundTrip(req *http.Request) (*http.Response, error) {
-	buf, _ := ioutil.ReadAll(req.Body)
-	req.Body = ioutil.NopCloser(bytes.NewBuffer(buf))
 
-	token, err := jose.Sign(string(buf), jose.HS256, c.sharedSecret)
+	// Read in the response from the api call.
+	b, err := ioutil.ReadAll(req.Body)
 	if err != nil {
 		return nil, err
 	}
 
+	// Replace the request Body with this new NopCloser.
+	req.Body = ioutil.NopCloser(bytes.NewBuffer(b))
+
+	// Produce a signed JWT token from our payload, using HS256 and the
+	// shared secret.
+	token, err := jose.Sign(string(b), jose.HS256, c.sharedSecret)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the JWT inside the request.
 	req.Header.Set("x-signature", token)
-	res, err := c.transporter.RoundTrip(req)
-	return res, err
+
+	// Perform the request with the token and payload.
+	return c.transporter.RoundTrip(req)
 }
