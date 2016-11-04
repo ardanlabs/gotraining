7933c72844e25a77c0c0019e390808c4720131f3
William Kennedy <bill@ardanlabs.com>
2016-11-04 12:18:20 -0400

    Refactored the web sockets code.

--- topics/web/sockets/README.md
+++ topics/web/sockets/README.md
@@ -2,17 +2,27 @@
 
 The web is changing and users are expecting fast, dynamic, and interactive web applications. Web Sockets allow for direct two-way communication between the front-end (JavaScript/HTML) and the back-end (Go).
 
-Standard Libary:
-[Code](example1/main.go)
-[Test](example1/main_test.go)
+## Notes
 
-Gorilla Websocket:
-[Code](example2/main.go)
-[Test](example2/main_test.go)
+* The standard library has much of what you need to build services and apps.
+* The http package provides the building blocks.
+* There are other great packages in the Go ecosystem to help.
 
 ## Links
-[http://www.gorillatoolkit.org/pkg/websocket](http://www.gorillatoolkit.org/pkg/websocket)
 
+https://golang.org/pkg/net/http/  
+https://golang.org/doc/articles/wiki/  
+http://www.gorillatoolkit.org/pkg/websocket  
 
+## Code Review
+
+Google websockets: [Code](example1/main.go) | [Test](example1/main_test.go)  
+Gorilla websockets: [Code](example2/main.go) | [Test](example2/main_test.go)  
+
+## Exercises
+
+### Exercise 1
+
+TBD
 ___
 All material is licensed under the [Apache License Version 2.0, January 2004](http://www.apache.org/licenses/LICENSE-2.0).
--- topics/web/sockets/example1/main.go
+++ topics/web/sockets/example1/main.go
@@ -1,3 +1,8 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Sample program on how to use the Google web socket package
+// to bind HTTP requests.
 package main
 
 import (
@@ -9,67 +14,96 @@ import (
 	"strings"
 	"time"
 
-	"golang.org/x/net/websocket"
-
 	"github.com/gorilla/pat"
+	"golang.org/x/net/websocket"
 )
 
+// Message represents the request we will receive on
+// the web socket connection.
 type Message struct {
 	Original  string    `json:"original"`
 	Formatted string    `json:"formatted"`
 	Received  time.Time `json:"received"`
 }
 
-func SocketHandler(ws *websocket.Conn) {
-	enc := json.NewEncoder(ws)
+// socketHandler is created for each connect that is
+// established on the server.
+func socketHandler(ws *websocket.Conn) {
+
+	log.Println("Connection established")
+	defer log.Println("Connection dropped")
+
+	// Create a buffer for use processing messages.
+	msg := make([]byte, 512)
+
+	// Maintain a read loop until the connection is
+	// broken or lost.
 	for {
-		msg := make([]byte, 512)
-		read, err := ws.Read(msg)
+
+		// Read a frame of data from the WebSocket connection.
+		// If msg is not large enough for the frame data, it fills
+		// the msg and next Read will read the rest of the frame data.
+		n, err := ws.Read(msg)
 		if err != nil {
 			log.Println(err)
 			break
 		}
 
-		if read > 0 {
-			m := string(msg[:read])
-			message := Message{
-				Original:  m,
-				Formatted: strings.ToUpper(m),
+		// If we received any data process it.
+		if n > 0 {
+
+			// Convert the bytes we received to a string.
+			data := string(msg[:n])
+
+			// Create a message and store the data.
+			msg := Message{
+				Original:  data,
+				Formatted: strings.ToUpper(data),
 				Received:  time.Now(),
 			}
-			enc.Encode(message)
+
+			// Encode the message to JSON and send it back.
+			if err := json.NewEncoder(ws).Encode(&msg); err != nil {
+				log.Println(err)
+				break
+			}
 		}
 	}
 }
 
+// App returns a handler for handling requets with JWT.
 func App() http.Handler {
+
+	// Create a new Pat router.
 	r := pat.New()
 
+	// Bind a GET call for the `/socket` route. This will establish
+	// a web socket connection.
 	r.Get("/socket", func(res http.ResponseWriter, req *http.Request) {
-		websocket.Handler(SocketHandler).ServeHTTP(res, req)
+		websocket.Handler(socketHandler).ServeHTTP(res, req)
 	})
+
 	// Bind the route for serving static files using the
 	// default FileServer. This will load the home page.
 	// r.Handle("/", http.FileServer(http.Dir(staticDir())))
 	r.Get("/", func(res http.ResponseWriter, req *http.Request) {
-		http.FileServer(http.Dir(staticDir())).ServeHTTP(res, req)
+		http.FileServer(http.Dir(currentDirectory())).ServeHTTP(res, req)
 	})
 
 	return r
 }
 
-// staticDir builds a full path to the 'static' directory
-// that is relative to this file.
-func staticDir() string {
+// Returns the current directory we are running in.
+func currentDirectory() string {
 
-	// Locate from the runtime the location of
-	// the apps static files.
-	_, filename, _, _ := runtime.Caller(1)
-
-	// Return a path to the static folder.
-	return path.Join(path.Dir(filename), "static")
+	// Locate the current directory for the site.
+	_, fn, _, _ := runtime.Caller(1)
+	return path.Join(path.Dir(fn), "static")
 }
 
 func main() {
+
+	// Start the http server to handle the request for
+	// both versions of the API.
 	log.Fatal(http.ListenAndServe(":3000", App()))
 }
