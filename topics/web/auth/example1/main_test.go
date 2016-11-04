975dc605855847bc526a5e0d8e62e1796c290fdd
William Kennedy <bill@ardanlabs.com>
2016-11-04 13:23:33 -0400

    Refactored the auth topic.

--- topics/web/auth/example1/main_test.go
+++ topics/web/auth/example1/main_test.go
@@ -1,90 +1,75 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Tests for the sample program to show how to apply basic
+// authentication to your web request.
 package main
 
 import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-func TestIndexHandlerAuthorized(t *testing.T) {
+func TestIndexHandler(t *testing.T) {
+	tests := []struct {
+		Username   string
+		Password   string
+		Want       string
+		StatusCode int
+	}{
+		{"username", "password", "Welcome Authorized User!", http.StatusOK},
+		{"badusername", "badpassword", "Not authorized", http.StatusUnauthorized},
+		{"", "", "Not authorized", http.StatusUnauthorized},
+	}
+
+	// Startup a server to handle processing these routes.
 	ts := httptest.NewServer(App())
 	defer ts.Close()
 
-	req, err := http.NewRequest("GET", ts.URL, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	req.SetBasicAuth("username", "password")
+	for _, tt := range tests {
 
-	c := http.Client{}
-	res, err := c.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
+		// Create a new request for the GET call.
+		req, err := http.NewRequest("GET", ts.URL, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	b, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
+		// Only apply the credentials if we have them.
+		if tt.Username != "" {
 
-	want := "Welcome Authorized User!"
-	got := string(b)
+			// Set the username and password into the request.
+			req.SetBasicAuth(tt.Username, tt.Password)
+		}
 
-	if want != got {
-		t.Logf("Wanted: %s", want)
-		t.Logf("Got   : %s", got)
-		t.Fail()
-	}
-}
+		// Create a Client and perform the GET call.
+		var c http.Client
+		res, err := c.Do(req)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-func TestIndexHandlerInvalidCredentials(t *testing.T) {
-	ts := httptest.NewServer(App())
-	defer ts.Close()
+		if res.StatusCode != tt.StatusCode {
+			t.Log("Wanted:", tt.StatusCode)
+			t.Log("Got   :", res.StatusCode)
+			t.Fatal("Mismatch")
+		}
 
-	req, err := http.NewRequest("GET", ts.URL, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	req.SetBasicAuth("badusername", "badpassword")
+		// Read in the response from the api call.
+		b, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	c := http.Client{}
-	res, err := c.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	want := http.StatusUnauthorized
-	got := res.StatusCode
-
-	if want != got {
-		t.Logf("Wanted: %s", want)
-		t.Logf("Got   : %s", got)
-		t.Fail()
-	}
-}
-
-func TestIndexHandlerNoCredentials(t *testing.T) {
-	ts := httptest.NewServer(App())
-	defer ts.Close()
-
-	req, err := http.NewRequest("GET", ts.URL, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	c := http.Client{}
-	res, err := c.Do(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	want := http.StatusUnauthorized
-	got := res.StatusCode
-
-	if want != got {
-		t.Logf("Wanted: %s", want)
-		t.Logf("Got   : %s", got)
-		t.Fail()
+		// Validate we received the expected response.
+		got := strings.TrimSpace(string(b))
+		want := tt.Want
+		if got != want {
+			t.Log("Wanted:", want)
+			t.Log("Got   :", got)
+			t.Fatal("Mismatch")
+		}
 	}
 }
