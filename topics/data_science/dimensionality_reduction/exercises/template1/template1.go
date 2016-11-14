c689cf96613325355fdc86b4ad127e10d19f5554
Daniel Whitenack <whitenack.daniel@gmail.com>
2016-11-14 16:31:10 -0500

    PCA exercise and notes

---
+++ topics/data_science/dimensionality_reduction/exercises/template1/template1.go
@@ -0,0 +1,63 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// go build
+// ./template1
+
+// Sample program to project iris data on to 3 principle components.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/gonum/matrix/mat64"
+	"github.com/kniren/gota/data-frame"
+)
+
+func main() {
+
+	// Pull in the CSV data.
+	irisData, err := ioutil.ReadFile("../../data/iris.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a dataframe from the CSV string.
+	// The types of the columns will be inferred.
+	irisDF := df.ReadCSV(string(irisData))
+
+	// Sequentially move the columns into a slice of floats.
+	floatData := make([]float64, 4*irisDF.Nrow())
+	var dataIndex int
+	for colIndex, colName := range irisDF.Names() {
+
+		// If the column is one of the float columns, move it
+		// into the slice of floats.
+		if colIndex < 4 {
+
+			// Extract the columns as a slice of floats.
+			floatCol, ok := irisDF.Col(colName).Elements.(df.FloatElements)
+			if !ok {
+				log.Fatal(fmt.Errorf("Could not parse float column."))
+			}
+
+			// Append the float values to floatData.
+			for _, floatVal := range floatCol {
+				floatData[dataIndex] = *floatVal.Float()
+				dataIndex++
+			}
+		}
+	}
+
+	// Form the matrix.
+	mat := mat64.NewDense(irisDF.Nrow(), 4, floatData)
+
+	// Calculate the principal component direction vectors
+	// and variances.
+
+	// Project the data onto the first 3 principal components.
+
+	// Output the resulting projected features to standard out.
+}
