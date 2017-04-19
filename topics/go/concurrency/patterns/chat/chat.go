b3f521c54b64a9fcd200086f1edb43e4e4b6a4df
William Kennedy <bill@ardanlabs.com>
2017-04-18 19:39:34 -0700

    Fixing comment spacing.

--- topics/go/concurrency/patterns/chat/chat.go
+++ topics/go/concurrency/patterns/chat/chat.go
@@ -137,12 +137,15 @@ func (r *Room) start() {
 		for {
 			select {
 			case message := <-r.outgoing:
+
 				// Sent message to the group.
 				r.sendGroupMessage(message)
 			case conn := <-r.joining:
+
 				// Join this connection to the room.
 				r.join(conn)
 			case <-r.shutdown:
+
 				// Chatroom shutting down.
 				r.wg.Done()
 				return
@@ -162,6 +165,7 @@ func (r *Room) start() {
 		for {
 			conn, err := r.listener.Accept()
 			if err != nil {
+
 				// Check if the error is temporary or not.
 				if e, ok := err.(temporary); ok {
 					if !e.Temporary() {
