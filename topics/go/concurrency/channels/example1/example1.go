e2bb85c0efec42a1faa8428eab9e89306c231c29
William Kennedy <bill@ardanlabs.com>
2017-10-20 16:32:19 -0400

    better examples for channels

--- topics/go/concurrency/channels/example1/example1.go
+++ topics/go/concurrency/channels/example1/example1.go
@@ -16,104 +16,128 @@ func init() {
 }
 
 func main() {
-	// sendRecv()
-	// signalClose()
-	// closeRange()
+	// waitForTask()
+	// waitForResult()
+	// waitForFinished()
+	// pooling()
 	// fanOut()
 	// fanOutSem()
-	// selectCancel()
-	// selectDrop()
+	// drop()
+	// cancellation()
 }
 
-type data struct {
-	UserID string
-}
-
-// sendRecv shows the basics of a send and receive.
-func sendRecv() {
-	fmt.Println("** basicSendRecv")
-
-	ch := make(chan data)
+// waitForTask: Think about being a manager and hiring a new employee. In
+// this scenario, you want your new employee to perform a task but they need
+// to wait until you are ready. This is because you need to hand them a piece
+// of paper before they start.
+func waitForTask() {
+	ch := make(chan string)
 
 	go func() {
-		time.Sleep(100 * time.Millisecond)
-		ch <- data{"123"}
-		fmt.Println("g2 : send ack")
+		p := <-ch
+		fmt.Println("employee : recv'd signal :", p)
 	}()
 
-	v := <-ch
-	fmt.Println("g1 : received :", v)
+	time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+	ch <- "paper"
+	fmt.Println("manager : sent signal")
 
 	time.Sleep(time.Second)
 	fmt.Println("-------------------------------------------------------------")
 }
 
-// signalClose shows how to close a channel to signal an event.
-func signalClose() {
-	fmt.Println("** signalClose")
+// waitForResult: Think about being a manager and hiring a new employee. In
+// this scenario, you want your new employee to perform a task immediately when
+// they are hired, and you need to wait for the result of their work. You need
+// to wait because you need the paper from them before you can continue.
+func waitForResult() {
+	ch := make(chan string)
 
+	go func() {
+		time.Sleep(100 * time.Millisecond)
+		ch <- "paper"
+		fmt.Println("employee : sent signal")
+	}()
+
+	p := <-ch
+	fmt.Println("manager : recv'd signal :", p)
+
+	time.Sleep(time.Second)
+	fmt.Println("-------------------------------------------------------------")
+}
+
+// waitForFinished: Think about being a manager and hiring a new employee. In
+// this scenario, you want your new employee to perform a task immediately when
+// they are hired, and you need to wait for the result of their work. You need
+// to wait because you can't move on until you know they are but you don't need
+// anything from them.
+func waitForFinished() {
 	ch := make(chan struct{})
 
 	go func() {
 		time.Sleep(100 * time.Millisecond)
 		close(ch)
-		fmt.Println("g2 : close ack")
+		fmt.Println("employee : sent signal")
 	}()
 
 	_, wd := <-ch
-	fmt.Println("g1 : received :", wd)
+	fmt.Println("manager : recv'd signal :", wd)
 
 	time.Sleep(time.Second)
 	fmt.Println("-------------------------------------------------------------")
 }
 
-// closeRange shows how to use range to receive values and
-// how using close terminates the loop.
-func closeRange() {
-	fmt.Println("** closeRange")
-
-	ch := make(chan data)
+// pooling: Think about being a manager and hiring a team of employees. In
+// this scenario, you want your new employees to perform tasks but they need
+// to wait until you are ready. This is because you need to hand them a piece
+// of paper before they start.
+func pooling() {
+	ch := make(chan string)
 
 	const grs = 2
 	for g := 0; g < grs; g++ {
 		go func(id int) {
-			for v := range ch {
-				fmt.Println("g2 : received :", v, ": on :", id)
+			for p := range ch {
+				fmt.Printf("employee %d : recv'd signal : %s\n", id, p)
 			}
-			fmt.Println("g2 : received close : on :", id)
+			fmt.Printf("employee %d : recv'd shutdown signal\n", id)
 		}(g)
 	}
 
 	const reqs = 10
 	for i := 0; i < reqs; i++ {
-		ch <- data{fmt.Sprintf("%d", i)}
+		ch <- "paper"
+		fmt.Println("manager : sent signal :", i)
 	}
 
 	close(ch)
-	fmt.Println("g1 : close ack")
+	fmt.Println("manager : sent shutdown signal")
 
 	time.Sleep(time.Second)
 	fmt.Println("-------------------------------------------------------------")
 }
 
-// fanOut shows how to use the fan out pattern to get work
-// done concurrently.
+// fanOut: Think about being a manager and hiring a team of employees. In
+// this scenario, you want your new employees to perform a task immediately when
+// they are hired, and you need to wait for all the results of their work.
+// You need to wait because you need the paper from each of them before you
+// can continue.
 func fanOut() {
-	fmt.Println("** fanOut")
-
 	grs := 20
-	ch := make(chan data, grs)
+	ch := make(chan string, grs)
 
 	for g := 0; g < grs; g++ {
 		go func(g int) {
 			time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
-			ch <- data{fmt.Sprintf("%d", g)}
+			ch <- "paper"
+			fmt.Println("employee : sent signal :", g)
 		}(g)
 	}
 
 	for grs > 0 {
-		d := <-ch
-		fmt.Println(d)
+		p := <-ch
+		fmt.Println(p)
+		fmt.Println("manager : recv'd signal :", grs)
 		grs--
 	}
 
@@ -121,13 +145,14 @@ func fanOut() {
 	fmt.Println("-------------------------------------------------------------")
 }
 
-// fanOutSem shows how to use the fan out pattern to get work
-// done concurrently but limiting the number of active routines.
+// fanOutSem: Think about being a manager and hiring a team of employees. In
+// this scenario, you want only some of the new employees performing a task
+// immediately when they are hired. The rest wait until other employees finish.
+// You need to wait for all the results of their work. You need to wait because
+// you need the paper from each of them before you can continue.
 func fanOutSem() {
-	fmt.Println("** fanOutSem")
-
 	grs := 20
-	ch := make(chan data, grs)
+	ch := make(chan string, grs)
 
 	const cap = 5
 	sem := make(chan bool, cap)
@@ -137,15 +162,17 @@ func fanOutSem() {
 			sem <- true
 			{
 				time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
-				ch <- data{fmt.Sprintf("%d", g)}
+				ch <- "paper"
+				fmt.Println("employee : sent signal :", g)
 			}
 			<-sem
 		}(g)
 	}
 
 	for grs > 0 {
-		d := <-ch
-		fmt.Println(d, len(sem))
+		p := <-ch
+		fmt.Println(p)
+		fmt.Println("manager : recv'd signal :", grs)
 		grs--
 	}
 
@@ -153,56 +180,63 @@ func fanOutSem() {
 	fmt.Println("-------------------------------------------------------------")
 }
 
-// selectCancel shows how to use the select statement to wait for a
-// specified amount of time to receive a value.
-func selectCancel() {
-	fmt.Println("** selectCancel")
-
-	ch := make(chan data, 1)
+// drop: Think about being a manager and hiring a new employee. In
+// this scenario, you want your new employee to perform a task but they need
+// to wait until you are ready. As the employee finishes their task you don’t
+// care to know they are done. All that’s important is whether you can or can’t
+// send new work. If you can’t perform the send, then you know your employee is
+// at capacity. At this point the new work needs to be discarded so things can
+// keep moving.
+func drop() {
+	const cap = 5
+	ch := make(chan string, cap)
 
 	go func() {
-		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
-		ch <- data{"123"}
-		fmt.Println("g2 : send ack")
+		for p := range ch {
+			fmt.Println("employee : recv'd signal :", p)
+		}
 	}()
 
-	tc := time.After(100 * time.Millisecond)
-
-	select {
-	case v := <-ch:
-		fmt.Println("g1 : received :", v)
-	case t := <-tc:
-		fmt.Println("g1 : timed out :", t)
+	const reqs = 20
+	for r := 0; r < reqs; r++ {
+		select {
+		case ch <- "paper":
+			fmt.Println("manager : sent signal :", r)
+		default:
+			fmt.Println("manager : dropped data :", r)
+		}
 	}
+
+	close(ch)
+	fmt.Println("manager : sent shutdown signal")
 
 	time.Sleep(time.Second)
 	fmt.Println("-------------------------------------------------------------")
 }
 
-// selectDrop shows how to use the select to walk away from a channel
-// operation if it will immediately block.
-func selectDrop() {
-	fmt.Println("** selectDrop")
-
-	const cap = 5
-	ch := make(chan data, cap)
+// cancellation: Think about being a manager and hiring a new employee. In
+// this scenario, you want your new employee to perform a task immediately when
+// they are hired, and you need to wait for the result of their work. This time
+// you are not willing to wait for some unknown amount of time for the employee
+// to finish. You are on a discrete deadline and if the employee doesn’t finish
+// in time, you are not willing to wait.
+func cancellation() {
+	ch := make(chan string, 1)
 
 	go func() {
-		for v := range ch {
-			fmt.Println("g2 : received :", v)
-		}
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		ch <- "paper"
+		fmt.Println("employee : sent signal")
 	}()
 
-	const reqs = 20
-	for i := 0; i < reqs; i++ {
-		select {
-		case ch <- data{fmt.Sprintf("%d", i)}:
-			fmt.Println("g1 : send ack")
-		default:
-			fmt.Println("g1 : drop")
-		}
+	tc := time.After(100 * time.Millisecond)
+
+	select {
+	case p := <-ch:
+		fmt.Println("manager : recv'd signal :", p)
+	case t := <-tc:
+		fmt.Println("manager : timedout :", t)
 	}
-	close(ch)
 
 	time.Sleep(time.Second)
 	fmt.Println("-------------------------------------------------------------")
