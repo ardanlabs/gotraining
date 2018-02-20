67cd484bfbbedf83edd44f367de59eca93bbf64d
William Kennedy <bill@ardanlabs.com>
2018-02-20 22:56:44 +0100

    removing starter kit because of ardanlabs/service

--- starter-kits/http/cmd/apid/views/js/users.js
+++
@@ -1,6 +0,0 @@
-var usersApp = angular.module('usersApp', []);
-usersApp.controller('UsersCtrl', function ($scope, $http){
-	$http.get('/v1/users').success(function(data) {
-		$scope.users = data;
-	});
-});
