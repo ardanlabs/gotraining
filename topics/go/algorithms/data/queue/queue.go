dd8a5db09f6d45937943acb994c2a6e97cdafbb4
Dmytro Tananayskiy <dmitriyminer@gmail.com>
2019-01-02 19:05:31 +0200

    Fix typos (#286)

--- topics/go/algorithms/data/queue/queue.go
+++ topics/go/algorithms/data/queue/queue.go
@@ -41,12 +41,12 @@ func (q *Queue) Enqueue(data *Data) error {
 
 	// If the front of the queue is right behind the end or
 	// if the front is at the end of the capacity and the end
-	// is at the beginning of the capcity, the queue is full.
+	// is at the beginning of the capacity, the queue is full.
 	//  F  E  - Enqueue (Full) |  E        F - Enqueue (Full)
 	// [A][B][C]               | [A][B][C]
 	if q.front+1 == q.end ||
 		q.front == len(q.data) && q.end == 0 {
-		return errors.New("queue at capcity")
+		return errors.New("queue at capacity")
 	}
 
 	switch {
