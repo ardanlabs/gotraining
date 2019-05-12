a3f16abd78306612df7e95d8c989ca1328bc7ef6
William Kennedy <bill@ardanlabs.com>
2019-05-12 11:14:49 -0400

    fixing bad commit on this code

--- topics/go/profiling/trace/trace.go
+++ topics/go/profiling/trace/trace.go
@@ -41,8 +41,8 @@ func main() {
 	// pprof.StartCPUProfile(os.Stdout)
 	// defer pprof.StopCPUProfile()
 
-	trace.Start(os.Stdout)
-	defer trace.Stop()
+	// trace.Start(os.Stdout)
+	// defer trace.Stop()
 
 	docs := make([]string, 4000)
 	for i := range docs {
@@ -62,65 +62,43 @@ func main() {
 }
 
 func find(topic string, docs []string) int {
-	var found int32
-
-	g := runtime.NumCPU()
-	var wg sync.WaitGroup
-	wg.Add(g)
-
-	ch := make(chan string, g)
-
-	for i := 0; i < g; i++ {
-		go func() {
-			var lFound int32
-			defer func() {
-				atomic.AddInt32(&found, lFound)
-				wg.Done()
-			}()
-
-			for doc := range ch {
-				file := fmt.Sprintf("%s.xml", doc[:8])
-				f, err := os.OpenFile(file, os.O_RDONLY, 0)
-				if err != nil {
-					log.Printf("Opening Document [%s] : ERROR : %v", doc, err)
-					return
-				}
-
-				data, err := ioutil.ReadAll(f)
-				if err != nil {
-					f.Close()
-					log.Printf("Reading Document [%s] : ERROR : %v", doc, err)
-					return
-				}
-				f.Close()
-
-				var d document
-				if err := xml.Unmarshal(data, &d); err != nil {
-					log.Printf("Decoding Document [%s] : ERROR : %v", doc, err)
-					return
-				}
-
-				for _, item := range d.Channel.Items {
-					if strings.Contains(item.Title, topic) {
-						lFound++
-						continue
-					}
-
-					if strings.Contains(item.Description, topic) {
-						lFound++
-					}
-				}
-			}
-		}()
-	}
+	var found int
 
 	for _, doc := range docs {
-		ch <- doc
-	}
-	close(ch)
+		file := fmt.Sprintf("%s.xml", doc[:8])
+		f, err := os.OpenFile(file, os.O_RDONLY, 0)
+		if err != nil {
+			log.Printf("Opening Document [%s] : ERROR : %v", doc, err)
+			return 0
+		}
 
-	wg.Wait()
-	return int(found)
+		data, err := ioutil.ReadAll(f)
+		if err != nil {
+			f.Close()
+			log.Printf("Reading Document [%s] : ERROR : %v", doc, err)
+			return 0
+		}
+		f.Close()
+
+		var d document
+		if err := xml.Unmarshal(data, &d); err != nil {
+			log.Printf("Decoding Document [%s] : ERROR : %v", doc, err)
+			return 0
+		}
+
+		for _, item := range d.Channel.Items {
+			if strings.Contains(item.Title, topic) {
+				found++
+				continue
+			}
+
+			if strings.Contains(item.Description, topic) {
+				found++
+			}
+		}
+	}
+
+	return found
 }
 
 func findConcurrent(topic string, docs []string) int {
