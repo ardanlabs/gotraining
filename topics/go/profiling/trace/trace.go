6bc1a265c939d3d21c81a913378240aa40c15155
William Kennedy <bill@ardanlabs.com>
2019-07-18 10:18:37 -0400

    using defer to keep the code more idomatic

--- topics/go/profiling/trace/trace.go
+++ topics/go/profiling/trace/trace.go
@@ -70,14 +70,13 @@ func freq(topic string, docs []string) int {
 			log.Printf("Opening Document [%s] : ERROR : %v", doc, err)
 			return 0
 		}
+		defer f.Close()
 
 		data, err := ioutil.ReadAll(f)
 		if err != nil {
-			f.Close()
 			log.Printf("Reading Document [%s] : ERROR : %v", doc, err)
 			return 0
 		}
-		f.Close()
 
 		var d document
 		if err := xml.Unmarshal(data, &d); err != nil {
@@ -121,14 +120,13 @@ func freqConcurrent(topic string, docs []string) int {
 				log.Printf("Opening Document [%s] : ERROR : %v", doc, err)
 				return
 			}
+			defer f.Close()
 
 			data, err := ioutil.ReadAll(f)
 			if err != nil {
-				f.Close()
 				log.Printf("Reading Document [%s] : ERROR : %v", doc, err)
 				return
 			}
-			f.Close()
 
 			var d document
 			if err := xml.Unmarshal(data, &d); err != nil {
@@ -178,14 +176,13 @@ func freqConcurrentSem(topic string, docs []string) int {
 					log.Printf("Opening Document [%s] : ERROR : %v", doc, err)
 					return
 				}
+				defer f.Close()
 
 				data, err := ioutil.ReadAll(f)
 				if err != nil {
-					f.Close()
 					log.Printf("Reading Document [%s] : ERROR : %v", doc, err)
 					return
 				}
-				f.Close()
 
 				var d document
 				if err := xml.Unmarshal(data, &d); err != nil {
