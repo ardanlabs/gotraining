a9c0a1d6f31871f34f0f0b0854330daed34f96a0
Harrison Brock <harrison@harrisonbrock.com>
2020-03-12 16:53:34 -0400

    Refactor code with better comments.

--- topics/go/algorithms/strings/reverse/reverse.go
+++ topics/go/algorithms/strings/reverse/reverse.go
@@ -1,22 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	// Run ReverseString
+
+	// Run the reverse string function.
 	fmt.Println(ReverseString("Hello World"))
 }
 
 func ReverseString(str string) string {
-	// Create rune.
-	chars := []rune(str)
 
-	// Reverse String.
-	for start, end := 0, len(chars)-1; start < end; start, end = start+1, end-1 {
-		// Swap values.
-		chars[start], chars[end] = chars[end], chars[start]
+	// Here we create a slice of runes.
+	str_table := []rune(str)
+
+	// Here we create int that will be a pointer to the front of the str_table.
+	frontIndex := 0
+
+	// Here  we create int that will be a pointer to the end of the str_table.
+	 BackIndex := len(str_table) - 1
+
+	 // While frontIndex is less than BackIndex.
+	for frontIndex < BackIndex {
+
+		// Here we swap the values of the slice at the str_table[frontIndex] and str_table[BackIndex].
+		str_table[frontIndex], str_table[BackIndex] = str_table[BackIndex], str_table[frontIndex]
+
+		// Here we increase the value of frontIndex by 1.
+		frontIndex = frontIndex + 1
+
+		// Here we decrease the value of BackIndex by 1.
+		BackIndex = BackIndex - 1
 	}
 
-	// Return a new string in reverse order.
-	return string(chars)
+	// Here we return a new string in reverse order.
+	return string(str_table)
 }
