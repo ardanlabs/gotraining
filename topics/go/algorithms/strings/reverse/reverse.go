f9450ced477e42689d602614922a569e4182faf7
Harrison Brock <harrison@harrisonbrock.com>
2020-03-14 15:54:49 -0400

    Refactor comments.

--- topics/go/algorithms/strings/reverse/reverse.go
+++ topics/go/algorithms/strings/reverse/reverse.go
@@ -2,13 +2,16 @@ package strings
 
 func ReverseString(str string) string {
 
-	// Create a slice of runes.
+	// Convert the input string into slice of runes for processing.
+	// A runes are unicode code-points.
 	runes := []rune(str)
 
-	// Create an index that will traverse the collection of runes from the beginning to the end.
+	// Create an index that will traverse the collection of
+	// runes from the beginning to the end.
 	var beg int
 
-	// Create an index that will traverse the collection of runes from the end to the beginning.
+	// Create an index that will traverse the collection of
+	// runes from the end to the beginning.
 	end := len(runes) - 1
 
 	// Keep swapping runes until the two indexes meet in the middle.
@@ -19,7 +22,8 @@ func ReverseString(str string) string {
 		runes[beg] = runes[end]
 		runes[end] = r
 
-		// Move the indexes closer to each other working towards the middle of the collection.
+		// Move the indexes closer to each other
+		// working towards the middle of the collection.
 		beg = beg + 1
 		end = end - 1
 
