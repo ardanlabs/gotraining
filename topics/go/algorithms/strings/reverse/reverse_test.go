b6a8309e02a87610f5cf5454ca08cf90295aa78e
Harrison Brock <harrison@harrisonbrock.com>
2020-03-14 16:29:09 -0400

    Added more more test cases.

--- topics/go/algorithms/strings/reverse/reverse_test.go
+++ topics/go/algorithms/strings/reverse/reverse_test.go
@@ -11,18 +11,20 @@ const failed = "\u2717"
 func TestReverseString(t *testing.T) {
 
 	revTests := []struct {
-		name   string
-		input  string
-		output string
+		name     string
+		input    string
+		expected string
 	}{
-		{"basic", "Hello World", "dlroW olleH"},
+		{"odd-string-length", "Hello World", "dlroW olleH"},
+		{"even-string-length", "go", "og"},
+		{"chinese", "汉字", "字汉"},
 	}
 
 	for _, tt := range revTests {
-		actual := strings.ReverseString(tt.input)
-		if actual != tt.output {
+		got := strings.ReverseString(tt.input)
+		if got != tt.expected {
 			t.Logf("\t%s\tShould be able to reverse string: %s\n.", failed, tt.input)
-			t.Fatalf("\t\tGot %s, Expected %s.", actual, tt.output)
+			t.Fatalf("\t\tGot %s, Expected %s.", got, tt.expected)
 		}
 		t.Logf("\t%s\tShould be able to reverse string.", succeed)
 	}
