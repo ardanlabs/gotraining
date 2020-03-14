da117e754c82110aa0c1311b81ddb5c2587ff3b6
Harrison Brock <harrison@harrisonbrock.com>
2020-03-13 20:54:22 -0400

    Refactor and cleaned up code.

--- topics/go/algorithms/strings/reverse/reverse.go
+++ topics/go/algorithms/strings/reverse/reverse.go
@@ -11,28 +11,29 @@ func main() {
 
 func ReverseString(str string) string {
 
-	// Here we create a slice of runes.
-	str_table := []rune(str)
+	// Here we create a slice of codePoints.
+	codePoints := []rune(str)
 
-	// Here we create int that will be a pointer to the front of the str_table.
-	frontIndex := 0
+	// Here we create int that will be a pointer to the front of the codePoints.
+	var fontCodePoint int
 
-	// Here  we create int that will be a pointer to the end of the str_table.
-	BackIndex := len(str_table) - 1
+	// Here  we create int that will be a pointer to the end of the codePoints.
+	backCodePoint := len(codePoints) - 1
 
-	// While frontIndex is less than BackIndex.
-	for frontIndex < BackIndex {
+	// While fontCodePoint is less than backCodePoint.
+	for fontCodePoint < backCodePoint {
 
-		// Here we swap the values of the slice at the str_table[frontIndex] and str_table[BackIndex].
-		str_table[frontIndex], str_table[BackIndex] = str_table[BackIndex], str_table[frontIndex]
+		// Here we swap the values of the slice at the codePoints[fontCodePoint] and codePoints[backCodePoint].
+		codePoints[fontCodePoint], codePoints[backCodePoint] = codePoints[backCodePoint], codePoints[fontCodePoint]
 
-		// Here we increase the value of frontIndex by 1.
-		frontIndex = frontIndex + 1
+		// Here we increase the value of fontCodePoint by 1.
+		fontCodePoint = fontCodePoint + 1
 
-		// Here we decrease the value of BackIndex by 1.
-		BackIndex = BackIndex - 1
+		// Here we decrease the value of backCodePoint by 1.
+		backCodePoint = backCodePoint - 1
 	}
 
 	// Here we return a new string in reverse order.
-	return string(str_table)
+	return string(codePoints)
 }
+
