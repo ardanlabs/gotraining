3853217c587a3e9b479d987c57405459404dcd94
Harrison Brock <harrison@harrisonbrock.com>
2020-03-18 14:30:02 -0400

    Add example is string a palindrome

---
+++ topics/go/algorithms/strings/palindrome/palindrome.go
@@ -0,0 +1,21 @@
+package strings
+
+import strings "github.com/ardanlabs/gotraining/topics/go/algorithms/strings/reverse"
+
+func IsPalindrome(input string) bool {
+
+	// If the input string is empty or as a length of 1 return true.
+	if input == "" || len(input) == 1 {
+		return true
+	}
+
+	// Create a reverse string from input string.
+	rev := strings.ReverseString(input)
+
+	// Check if input and rev strings are equal.
+	if input == rev {
+		return true
+	}
+
+	return false
+}
---
+++ topics/go/algorithms/strings/palindrome/palindrome_test.go
@@ -0,0 +1,35 @@
+package strings_test
+
+import (
+	strings "github.com/ardanlabs/gotraining/topics/go/algorithms/strings/palindrome"
+	"testing"
+)
+
+const succeed = "\u2713"
+const failed = "\u2717"
+
+func TestIsPalindrome(t *testing.T) {
+
+	revTests := []struct {
+		name     string
+		input    string
+		expected bool
+	}{
+		{"empty string", "", true},
+		{"string with length of 1", "G", true},
+		{"string with odd length", "bob", true},
+		{"string with even length", "otto", true},
+		{"chinese", "汉字汉", true},
+		{"failed test", "test", false},
+	}
+
+	for _, tt := range revTests {
+		got := strings.IsPalindrome(tt.input)
+		if got {
+			t.Logf("\t%s\tString is a palindrome.", succeed)
+		} else {
+			t.Logf("\t%s\tString is a palindrome: %s\n.", failed, tt.input)
+			t.Fatalf("\t\tGot %v, Expected %v.", got, tt.expected)
+		}
+	}
+}
