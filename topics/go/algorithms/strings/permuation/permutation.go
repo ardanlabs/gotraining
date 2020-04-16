0728ec842fbde65115e1a0a255b62b4a93d4c6a8
Harrison Brock <30420585+harrisonbrock@users.noreply.github.com>
2020-04-16 14:05:16 -0400

    Permutation Example (#312)
    * Added example for checking if two strings are a Permutation
    
    * Fixed comments and formatting.
    
    * Fixed formatting.
    
    * Fixed formatting.
    
    * Added the RuneSlice from Bill Kennedy.
    
    * Refactored the code to use the RuneSlice.
    
    * Move RuneSlice back into the permutation.go file.
    Fixed comment.
    
    * Fixed comments.
    
    * Fixed typo

---
+++ topics/go/algorithms/strings/permuation/permutation.go
@@ -0,0 +1,34 @@
+package strings
+
+import (
+	"sort"
+)
+
+// RuneSlice a custom type of a slice of runes.
+type RuneSlice []rune
+
+// For sorting an RuneSlice.
+func (p RuneSlice) Len() int           { return len(p) }
+func (p RuneSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p RuneSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+// IsPermutation check if two strings are permutations.
+func IsPermutation(str1, str2 string) bool {
+
+	// If the length are not equal they cannot be permutation.
+	if len(str1) != len(str2) {
+		return false
+	}
+
+	// Convert each string into a collection of runes.
+	s1 := []rune(str1)
+	s2 := []rune(str2)
+
+	// Sort each collection of runes.
+	sort.Sort(RuneSlice(s1))
+	sort.Sort(RuneSlice(s2))
+
+	// Convert the collection of runes back to a string
+	// and compare.
+	return string(s1) == string(s2)
+}
---
+++ topics/go/algorithms/strings/permuation/permutation_test.go
@@ -0,0 +1,33 @@
+package strings_test
+
+import (
+	strings "github.com/ardanlabs/gotraining/topics/go/algorithms/strings/permuation"
+	"testing"
+)
+
+const succeed = "\u2713"
+const failed = "\u2717"
+
+func TestIsPermutation(t *testing.T) {
+
+	permutationTests := []struct {
+		name     string
+		input    string
+		input2   string
+		expected bool
+	}{
+		{"empty string", "", "", true},
+		{"old number of string test", "god", "dog", true},
+		{"different size inputs", "god", "do", false},
+		{"binary string (even number of strings)", "1001", "0110", true},
+	}
+
+	for _, tt := range permutationTests {
+		got := strings.IsPermutation(tt.input, tt.input2)
+		if got != tt.expected {
+			t.Logf("\t%s\tString is a palindrome: %s\n.", failed, tt.input)
+			t.Fatalf("\t\tGot %v, Expected %v.", got, tt.expected)
+		}
+		t.Logf("\t%s\tString %s is a palindrome.", succeed, tt.input)
+	}
+}
