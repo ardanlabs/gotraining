0696a9b1cb2a1a2a64717ffcddc47097c6605194
Harrison Brock <harrison@harrisonbrock.com>
2020-04-16 13:18:41 -0400

    Move RuneSlice back into the permutation.go file. Fixed comment.

--- topics/go/algorithms/strings/permuation/permutation.go
+++ topics/go/algorithms/strings/permuation/permutation.go
@@ -1,9 +1,15 @@
 package strings
 
 import (
-	strings "github.com/ardanlabs/gotraining/topics/go/algorithms/strings/types"
 	"sort"
 )
+
+type RuneSlice []rune
+
+// For sorting an RuneSlice
+func (p RuneSlice) Len() int           { return len(p) }
+func (p RuneSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p RuneSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 // IsPermutation check if two strings are permutations.
 func IsPermutation(str1, str2 string) bool {
@@ -17,9 +23,9 @@ func IsPermutation(str1, str2 string) bool {
 	s1 := []rune(str1)
 	s2 := []rune(str2)
 
-	// Sort the the two runes
-	sort.Sort(strings.RuneSlice(s1))
-	sort.Sort(strings.RuneSlice(s2))
+	// Sort the the two runes.
+	sort.Sort(RuneSlice(s1))
+	sort.Sort(RuneSlice(s2))
 
 	return string(s1) == string(s2)
 }
--- topics/go/algorithms/strings/types/types.go
+++
@@ -1,8 +0,0 @@
-package strings
-
-type RuneSlice []rune
-
-// For sorting an RuneSlice
-func (p RuneSlice) Len() int           { return len(p) }
-func (p RuneSlice) Less(i, j int) bool { return p[i] < p[j] }
-func (p RuneSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
