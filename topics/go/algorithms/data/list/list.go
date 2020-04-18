26584e0c78aff871be20192ad39666d9335d68ff
Harrison Brock <harrison@harrisonbrock.com>
2020-04-17 23:17:38 -0400

    FIXED: Typo

--- topics/go/algorithms/data/list/list.go
+++ topics/go/algorithms/data/list/list.go
@@ -1,7 +1,7 @@
 // All material is licensed under the Apache License Version 2.0, January 2004
 // http://www.apache.org/licenses/LICENSE-2.0
 
-// Package list implements of a doule link list in Go.
+// Package list implements of a doubly link list in Go.
 package list
 
 import (
