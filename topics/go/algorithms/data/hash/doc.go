af77b140b1e0911f07e6569c8469e886bd0c81e7
Miki Tebeka <miki.tebeka@gmail.com>
2020-04-20 16:42:33 +0300

    wikipedia link

--- topics/go/algorithms/data/hash/doc.go
+++ topics/go/algorithms/data/hash/doc.go
@@ -27,4 +27,5 @@ Hash table
 - hashKey(key) returns a number between 0 to len(buckets)-1
 - We use a slice of entries as a bucket to handles cases where two or more keys
   are hashed to the same bucket
+- See more at https://en.wikipedia.org/wiki/Hash_table
 */
