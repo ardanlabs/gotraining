2ee021f1bbd6ca0d7d1409fded4fe178a56bf188
Harrison Brock <harrison@harrisonbrock.com>
2020-04-21 14:24:32 -0400

    Added test for min function

--- topics/go/algorithms/data/bst/bst_test.go
+++ topics/go/algorithms/data/bst/bst_test.go
@@ -37,3 +37,34 @@ func TestMax(t *testing.T) {
 		}
 	}
 }
+
+func TestMin(t *testing.T) {
+
+	t.Log("Given the need to test the bst functionality")
+	{
+		testID := 0
+		b := bst.New()
+		t.Logf("\tTest %d:\tWhen getting min.", testID)
+		{
+			b.Insert(50)
+			b.Insert(30)
+
+			got, _ := b.Min()
+			if got != 30 {
+				t.Errorf("\t%s\tTest %d:\tShould have the correct min", failed, testID)
+				t.Fatalf("\t\tTest %d:\tGot: %d, Expected: %d", testID, got, 30)
+			}
+		}
+		testID = 1
+		t.Logf("\tTest %d: \tWhen getting max after adding new int.", testID)
+		{
+			b.Insert(25)
+
+			got, _ := b.Min()
+			if got != 25 {
+				t.Errorf("\t%s\tTest %d:\tShould have the correct min", failed, testID)
+				t.Fatalf("\t\tTest %d:\tGot: %d, Expected: %d", testID, got, 25)
+			}
+		}
+	}
+}
