4509d05a708ec3676250978a9e2e44f65993648b
Harrison Brock <harrison@harrisonbrock.com>
2020-04-21 23:17:52 -0400

    Added tests for search.

--- topics/go/algorithms/data/bst/bst_test.go
+++ topics/go/algorithms/data/bst/bst_test.go
@@ -23,6 +23,7 @@ func TestMax(t *testing.T) {
 				t.Errorf("\t%s\tTest %d:\tShould have the correct max", failed, testID)
 				t.Fatalf("\t\tTest %d:\tGot: %d, Expected: %d", testID, got, 5)
 			}
+			t.Logf("\t%s\tTest %d:\tShould have the correct min", succeed, testID)
 		}
 		testID = 1
 		t.Logf("\tTest %d: \tWhen getting max after adding new int.", testID)
@@ -34,6 +35,7 @@ func TestMax(t *testing.T) {
 				t.Errorf("\t%s\tTest %d:\tShould have the correct max", failed, testID)
 				t.Fatalf("\t\tTest %d:\tGot: %d, Expected: %d", testID, got, 5)
 			}
+			t.Logf("\t%s\tTest %d:\tShould have the correct min", succeed, testID)
 		}
 	}
 }
@@ -54,6 +56,7 @@ func TestMin(t *testing.T) {
 				t.Errorf("\t%s\tTest %d:\tShould have the correct min", failed, testID)
 				t.Fatalf("\t\tTest %d:\tGot: %d, Expected: %d", testID, got, 30)
 			}
+			t.Logf("\t%s\tTest %d:\tShould have the correct min", succeed, testID)
 		}
 		testID = 1
 		t.Logf("\tTest %d: \tWhen getting max after adding new int.", testID)
@@ -65,6 +68,41 @@ func TestMin(t *testing.T) {
 				t.Errorf("\t%s\tTest %d:\tShould have the correct min", failed, testID)
 				t.Fatalf("\t\tTest %d:\tGot: %d, Expected: %d", testID, got, 25)
 			}
+			t.Logf("\t%s\tTest %d:\tShould have the correct min", succeed, testID)
 		}
+	}
+}
+
+func TestSearch(t *testing.T) {
+
+	t.Log("Given the need to test the bst functionality")
+	{
+		testID := 0
+		b := bst.New()
+		t.Logf("\tTest %d:\tWhen searching for value in tree.", testID)
+		{
+			b.Insert(50)
+			b.Insert(30)
+
+			got := b.Search(50)
+			if !got {
+				t.Errorf("\t%s\tTest %d:\tShould return false", failed, testID)
+				t.Fatalf("\t\tTest %d:\tGot: %v, Expected: %v", testID, got, true)
+			}
+			t.Logf("\t%s\tTest %d:\tShould return false", succeed, testID)
+		}
+		testID = 1
+		t.Logf("\tTest %d: \tWhen searching for a value not in the tree.", testID)
+		{
+
+			got := b.Search(25)
+			if got {
+				t.Errorf("\t%s\tTest %d:\tShould return false", failed, testID)
+				t.Fatalf("\t\tTest %d:\tGot: %v, Expected: %v", testID, got, false)
+			}
+			t.Logf("\t%s\tTest %d:\tShould return false", succeed, testID)
+
+		}
+
 	}
 }
