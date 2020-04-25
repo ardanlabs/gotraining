8c2f690fabe4c5fd5a358663505148bb813a9fd0
Harrison Brock <harrison@harrisonbrock.com>
2020-04-25 18:10:32 -0400

    Fixed typo

--- topics/go/algorithms/numbers/palindrome/palindrome.go
+++ topics/go/algorithms/numbers/palindrome/palindrome.go
@@ -5,7 +5,7 @@ import "github.com/ardanlabs/gotraining/topics/go/algorithms/numbers/reverse"
 // Is checks if a integer is a Palindrome.
 func Is(input int) bool {
 
-	// An negative integer is not a palindrome.
+	// A negative integer is not a palindrome.
 	if input < 0 {
 		return false
 	}
