8bb769f7a0d54880695ae366667ba5a681899240
Burhanettin Nacar <mtbnacar@gmail.com>
2021-07-08 17:34:39 +0300

    Update example1.go
    - fixed a comment as seven byte padding from four byte padding.
    - renamed fbp to svnbp.

--- topics/go/language/struct_types/advanced/example1/example1.go
+++ topics/go/language/struct_types/advanced/example1/example1.go
@@ -35,8 +35,8 @@ type tbp struct {
 	b int32 //	4 bytes				size 8 - Aligned on 2 bytes
 }
 
-// Four byte padding.
-type fbp struct {
+// Seven byte padding.
+type svnbp struct {
 	a bool //	1 byte				size 1
 	//			7 bytes padding		size 8
 	b int64 //	8 bytes				size 16 - Aligned on 8 bytes
@@ -93,9 +93,9 @@ func main() {
 	// =========================================================================
 
 	// structlayout -json github.com/ardanlabs/gotraining/topics/go/language/struct_types/advanced/example1 fbp | structlayout-pretty
-	var fbp fbp
-	size = unsafe.Sizeof(fbp)
-	fmt.Printf("SizeOf[%d][%p %p]\n", size, &fbp.a, &fbp.b)
+	var svnbp svnbp
+	size = unsafe.Sizeof(svnbp)
+	fmt.Printf("SizeOf[%d][%p %p]\n", size, &svnbp.a, &svnbp.b)
 
 	// =========================================================================
 
