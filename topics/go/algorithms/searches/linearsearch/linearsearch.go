ff39d5ed3e475764e72af82889cead0c59c085db
Milad Poshtdari <mposhtdari@gmail.com>
2021-09-16 08:31:41 +0430

    added double linearsearch algorithm with test

--- topics/go/algorithms/searches/linearsearch/linearsearch.go
+++ topics/go/algorithms/searches/linearsearch/linearsearch.go
@@ -5,7 +5,11 @@ package linearsearch
 // the best case of this algorithm is O(1)
 func linearSearchIterative(list []int, target int) int {
 
-	for i := range list{
+	if len(list) <= 0{
+		return -1
+	}
+
+	for i := range list {
 		if list[i] == target {
 			return i
 		}
@@ -17,7 +21,7 @@ func linearSearchIterative(list []int, target int) int {
 // linearSearchRecursive is the algorithm that search the value inside the array index by index with recall the function
 func linearSearchRecursive(list []int, target int, index int) int {
 
-	if len(list) <= index{
+	if len(list) <= index {
 		return -1
 	}
 
@@ -27,9 +31,65 @@ func linearSearchRecursive(list []int, target int, index int) int {
 		return index
 	}
 
-	if len(list) >= index{
+	if len(list) >= index {
 		index++ // increase by one
-		return linearSearchRecursive(list, target,index)
+		return linearSearchRecursive(list, target, index)
+	}
+
+	// if value is not found I returned -1
+	return -1
+}
+
+// doubleLinearSearchIterative is the algorithm that search the value inside the array in both side
+// the worst case of this algorithm is O(n)
+// the best case of this algorithm is O(1)
+func doubleLinearSearchIterative(list []int, target int) int {
+
+	var low int           // first index
+	high := len(list) - 1 // last index
+
+	if len(list) <= 0{
+		return -1
+	}
+
+	// continue loop until low is smaller or equal to high value
+	for low <= high {
+
+		if list[low] == target {
+			return low
+		}
+
+		if list[high] == target {
+			return high
+		}
+
+		low++
+		high--
+	}
+
+	// if value is not found I returned -1
+	return -1
+}
+
+// doubleLinearSearchRecursive is the algorithm that search the value inside the array in both side
+func doubleLinearSearchRecursive(list []int, target int, low int, high int) int {
+
+	if len(list) <= 0{
+		return -1
+	}
+
+	if list[low] == target {
+		return low
+	}
+
+	if list[high] == target {
+		return high
+	}
+
+	if low <= high {
+		low++
+		high--
+		return doubleLinearSearchRecursive(list, target, low, high)
 	}
 
 	// if value is not found I returned -1
--- topics/go/algorithms/searches/linearsearch/linearsearch_test.go
+++ topics/go/algorithms/searches/linearsearch/linearsearch_test.go
@@ -50,6 +50,45 @@ func TestLinearSearch(t *testing.T)  {
 			}
 		}
 	})
+
+
+
+	t.Run("Double Linear Search Iterative", func(t *testing.T) {
+		t.Log("Search to find the target value in both side")
+		{
+			result := doubleLinearSearchIterative(data.list, data.pick)
+
+			if result == -1 {
+				t.Errorf("excepted %d, but got -1", result)
+			}
+
+
+			result2 := doubleLinearSearchIterative(data.list, -10)
+			if result2 != -1 {
+				t.Errorf("excepted -1 , but got %d", result2)
+			}
+
+		}
+	})
+
+
+
+	t.Run("Double Linear Search Recursive", func(t *testing.T) {
+		t.Log("Search to find the target value in both side")
+		{
+			result := doubleLinearSearchRecursive(data.list, data.pick, 0, len(data.list) - 1)
+
+			if result == -1 {
+				t.Errorf("excepted %d, but got -1", result)
+			}
+
+			result2 := doubleLinearSearchRecursive(data.list, -10,0, len(data.list) - 1)
+			if result2 != -1 {
+				t.Errorf("excepted -1 , but got %d", result2)
+			}
+		}
+	})
+
 }
 
 // generateRandomList it generates a random list of numbers
