9701a9ce3b64ce7862a3df0aa5ad4d50c1866507
Milad Poshtdari <mposhtdari@gmail.com>
2021-09-28 20:47:46 +0330

    remove leading lines space from the beginning of each function

--- topics/go/algorithms/searches/linearsearch/linearsearch.go
+++ topics/go/algorithms/searches/linearsearch/linearsearch.go
@@ -7,7 +7,6 @@ package linearsearch
 // - the worst case of this algorithm is O(n).
 // - the best case of this algorithm is O(1).
 func linearSearchIterative(list []int, target int) int {
-
 	if len(list) <= 0 {
 		return -1
 	}
@@ -25,7 +24,6 @@ func linearSearchIterative(list []int, target int) int {
 // and uses the `recursive` process to check index by index to
 // find the target.
 func linearSearchRecursive(list []int, target int, index int) int {
-
 	switch {
 
 	case len(list) <= index:
@@ -46,7 +44,6 @@ func linearSearchRecursive(list []int, target int, index int) int {
 // and uses the `iterative` process to check index by index to find
 // the target in both left and right index.
 func doubleLinearSearchIterative(list []int, target int) int {
-
 	var leftIdx int
 	rightIdx := len(list) - 1
 
@@ -79,7 +76,6 @@ func doubleLinearSearchIterative(list []int, target int) int {
 // and uses the `recursive` process to check index by index to find
 // the target in both left and right index.
 func doubleLinearSearchRecursive(list []int, target int, leftIdx int, rightIdx int) int {
-
 	if len(list) > 0 {
 
 		switch {
