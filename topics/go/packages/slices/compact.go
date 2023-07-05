745949eae9128916fc288bcdb1b26ac3c10577c1
Cheikh Seck <cheikh.seck@ardanlabs.com>
2023-07-05 14:31:17 +0000

    Update compact, still testing

--- topics/go/packages/slices/compact.go
+++ topics/go/packages/slices/compact.go
@@ -1,1 +1,34 @@
+// This program showcases
+// the `slices` package's compact function.
+// The aim of this test is to remove consecutive duplicate
+// elements within a slice
+// This program requires Go 1.21rc1
 package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+
+	a := []int{
+		1, 2, 1, 1, 3, 4, 5,
+	}
+
+	fmt.Println("Original", a)
+	cache := map[int]bool{}
+	a = slices.CompactFunc(a, func(e1 int, e2 int) bool {
+
+		_, ok := cache[e1]
+
+		if ok {
+			return true
+		}
+
+		cache[e1] = true
+
+		return false
+	})
+	fmt.Println("Compact", a)
+}
