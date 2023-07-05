924a33cd3051e60da0ab1998a5017fdd8578bedb
Cheikh Seck <cheikh.seck@ardanlabs.com>
2023-07-05 15:27:13 +0000

    Add is sorted function.

---
+++ topics/go/packages/slices/is_sorted.go
@@ -0,0 +1,31 @@
+// This program showcases
+// the `slices` package's IsSorted function
+// to determine if an array is in ascending
+// order.
+// This program requires Go 1.21rc1
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+
+	a := []int{
+		1, 2, 3, 4, 5, 6,
+	}
+
+	b := []int{
+		1, 7, 2, 3, 4, 5, 6,
+	}
+
+	fmt.Println("Array A:", a)
+	fmt.Println("Array B:", b)
+
+	isSortedA := slices.IsSorted(a)
+	isSortedB := slices.IsSorted(b)
+
+	fmt.Println("Is in ascending order:", isSortedA)
+	fmt.Println("Is in ascending order:", isSortedB)
+}
