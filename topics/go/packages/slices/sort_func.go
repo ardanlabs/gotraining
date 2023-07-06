e40be6a3a1e609285db6703cfa34befa8b4557b8
Cheikh Seck <cheikh.seck@ardanlabs.com>
2023-07-06 11:18:52 +0000

    Add code for grow function.

--- topics/go/packages/slices/sort_func.go
+++ topics/go/packages/slices/sort_func.go
@@ -6,26 +6,6 @@
 // This program requires Go 1.21rc1
 package main
 
-import (
-	"fmt"
-	"slices"
-)
-
 func main() {
 
-	a := []int{
-		5, 4, 1, 1, 6, 3,
-	}
-
-	fmt.Println("Original", a)
-
-	a = slices.SortFunc(a, NumSort)
-
-	fmt.Println("Sorted", a)
-}
-
-type NumSort func(p1, p2 int) bool
-
-func (n NumSort) Sort(numbers []int) {
-	fmt.Println(numbers)
 }
