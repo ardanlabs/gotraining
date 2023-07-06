296e8e0a1aac694ae76a2a53b50b8a8c6f9f551e
Cheikh Seck <cheikh.seck@ardanlabs.com>
2023-07-06 11:12:41 +0000

    update compare function.

--- topics/go/packages/slices/compare.go
+++ topics/go/packages/slices/compare.go
@@ -1,7 +1,8 @@
 // This program showcases
 // the `slices` package's compare function.
-// The aim of this test is unclear
-// Need additional clarification
+// The aim of this test is to leverage
+// the compare function to determine
+// which array's length is greater
 // This program requires Go 1.21rc1
 package main
 
@@ -17,20 +18,39 @@ func main() {
 	}
 
 	b := []int{
-		1, 2, 6, 4, 5,
+		1, 2, 6, 4, 5, 6,
 	}
 
 	c := []int{
-		1, 2, 3, 4, 5,
+		1, 2, 3, 4,
 	}
 
+	d := []int{
+		1, 2, 3, 4,
+	}
+
+	// d is short for
+	// dictionary and translates
+	// the output from the compare
+	// function into something that is
+	// human readable.
+	dict := map[int]string{
+		-1: "First slice is shorter",
+		0:  "Both slices are equal",
+		1:  "Second slice is shorter",
+	}
 	fmt.Println(
 		"Compare Slice a and b",
-		slices.Compare(a, b),
+		dict[slices.Compare(a, b)],
 	)
 
 	fmt.Println(
 		"Compare Slice a and c",
-		slices.Compare(a, c),
+		dict[slices.Compare(a, c)],
+	)
+
+	fmt.Println(
+		"Compare Slice c and d",
+		dict[slices.Compare(c, d)],
 	)
 }
