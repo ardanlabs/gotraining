d122356ff529f0f5b62e41a4185c69c5c2aade2d
Cheikh Seck <cheikh.seck@ardanlabs.com>
2023-07-10 11:56:30 +0000

    add example for sort func.

--- topics/go/packages/slices/sort_func.go
+++ topics/go/packages/slices/sort_func.go
@@ -6,6 +6,49 @@
 // This program requires Go 1.21rc1
 package main
 
+import (
+	"fmt"
+	"slices"
+	"time"
+)
+
+type Order struct {
+	Date     time.Time
+	Name     string
+	Complete bool
+}
+
 func main() {
 
+	// This example demonstrates
+	// how sort func can be used
+	// with a slice of numbers.
+	ints := []int{
+		2, 4, 6, 7, 8, 9, 1, 0,
+	}
+
+	slices.SortFunc(ints, func(a, b int) bool { return a < b })
+
+	fmt.Println("Is slice int sorted", slices.IsSorted(ints))
+	fmt.Println("Resulting slice after sort", ints)
+
+	// This example showcases how the
+	// sort function can be leveraged
+	// to sort complex types.
+	orders := []Order{
+		Order{
+			Date:     time.Now(),
+			Name:     "Bob",
+			Complete: false,
+		},
+		Order{
+			Date:     time.Now(),
+			Name:     "Alice",
+			Complete: true,
+		},
+	}
+
+	slices.SortFunc(orders, func(a, b Order) bool { return a.Date.Unix() < b.Date.Unix() })
+
+	fmt.Println("Sorted slices of orders:", orders)
 }
