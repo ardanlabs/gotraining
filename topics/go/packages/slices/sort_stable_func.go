0c294648807c84d7051cd19b3980858b92439d7d
Cheikh Seck <cheikh.seck@ardanlabs.com>
2023-07-10 12:33:22 +0000

    correct sort func description. Add example for SortStableFunc

--- topics/go/packages/slices/sort_stable_func.go
+++ topics/go/packages/slices/sort_stable_func.go
@@ -1,8 +1,8 @@
 // This program showcases
-// the `slices` package's SortFunc function.
+// the `slices` package's SortStableFunc function.
 // The aim of this test is to define a sort function
 // and use it to sort a slice of integers in ascending
-// order and another to sort  a complex type.
+// order.
 // This program requires Go 1.21rc1
 package main
 
@@ -25,31 +25,12 @@ func main() {
 	// with a slice of numbers.
 	ints := []int{
 		2, 4, 6, 7, 8, 9, 1, 0,
-	}
+	}[:]
 
 	var less = func(a, b int) int { return a - b }
-	slices.SortFunc(ints, less)
+	slices.SortStableFunc(ints, less)
 
 	fmt.Println("Is slice int sorted", slices.IsSorted(ints))
 	fmt.Println("Resulting slice after sort", ints)
 
-	// This example showcases how the
-	// sort function can be leveraged
-	// to sort complex types.
-	orders := []Order{
-		Order{
-			Date:     time.Now().AddDate(0, 0, 2),
-			Name:     "Bob",
-			Complete: false,
-		},
-		Order{
-			Date:     time.Now(),
-			Name:     "Alice",
-			Complete: true,
-		},
-	}
-
-	slices.SortFunc(orders, func(a, b Order) int { return int(a.Date.Unix() - b.Date.Unix()) })
-
-	fmt.Println("Sorted slices of orders:", orders)
 }
