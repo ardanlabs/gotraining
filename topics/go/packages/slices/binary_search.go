6b19f846cd576d0c2c3e1b28c0404a8f1a84227b
Cheikh Seck <cheikh.seck@ardanlabs.com>
2023-07-10 07:31:06 +0000

    Add binary search.

---
+++ topics/go/packages/slices/binary_search.go
@@ -0,0 +1,39 @@
+// This program showcases
+// the `slices` package's binarySearch function
+// The aim of this program is to perform
+// a binary search,
+// a search on an array that is already
+// sorted in ascending order.
+// This program requires Go 1.21rc1
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+
+	a := []int{
+		1, 2, 4, 5, 6,
+	}
+
+	_, found := slices.BinarySearch(
+		a,
+		3,
+	)
+
+	if !found {
+		fmt.Println("3 not found in slice")
+	}
+
+	index, found := slices.BinarySearch(
+		a,
+		5,
+	)
+
+	if found {
+		fmt.Println("found element 5 at index:", index)
+	}
+
+}
