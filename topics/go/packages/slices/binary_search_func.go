1ac67f69819937ed501a5d3d8e93e5b8c49341cb
Cheikh Seck <cheikh.seck@ardanlabs.com>
2023-07-11 10:45:04 +0000

    Add binary search func example.

---
+++ topics/go/packages/slices/binary_search_func.go
@@ -0,0 +1,45 @@
+// This program showcases
+// the `slices` package's binarySearchFunc function
+// The aim of this program is to perform
+// a binary search,
+// a search on an array that is already
+// sorted in ascending order.
+// This program requires Go 1.21rc1
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+
+	ints := []int{
+		1, 2, 4, 5, 6,
+	}
+
+	cmp := func(a, b int) int {
+		return a - b
+	}
+
+	_, found := slices.BinarySearchFunc(
+		ints,
+		3,
+		cmp,
+	)
+
+	if !found {
+		fmt.Println("3 not found in slice")
+	}
+
+	index, found := slices.BinarySearchFunc(
+		ints,
+		5,
+		cmp,
+	)
+
+	if found {
+		fmt.Println("found element 5 at index:", index)
+	}
+
+}
