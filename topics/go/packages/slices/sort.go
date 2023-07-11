8295483798e10ac889152754bd811fd8fcbc5d63
Cheikh Seck <cheikh.seck@ardanlabs.com>
2023-07-11 12:49:42 +0000

    re-add sort example.

---
+++ topics/go/packages/slices/sort.go
@@ -0,0 +1,27 @@
+// This program showcases
+// the `slices` package's sort function.
+// The aim of this test is to sort an array of
+// integers to ascending order.
+// This program requires Go 1.21rc1
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+
+	// This example demonstrates
+	// how sort func can be used
+	// with a slice of numbers.
+	ints := []int{
+		2, 4, 6, 7, 8, 9, 1, 0,
+	}
+
+	fmt.Println("Original", ints)
+	ints = slices.Sort(ints)
+
+	fmt.Println("Sorted", ints)
+
+}
